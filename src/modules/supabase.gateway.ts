b57ce8d6ebfc0122161147bb4056152dd0795518
tramallo <timo.22394@hotmail.com>
2025-01-08 22:57:54 -0300

    supabaseGateway -> add logs

--- src/modules/supabase.gateway.ts
+++ src/modules/supabase.gateway.ts
@@ -13,6 +13,7 @@ export class SupabaseGateway {
         @MessageBody() data: any,
         @ConnectedSocket() client: Socket,
     ) {
+        console.log(`supabaseGatway -> properties-subscribe ${client.id}`);
         client.data.propertiesFilter = data;
     }
 
@@ -20,6 +21,7 @@ export class SupabaseGateway {
     propertiesUnsubscribe(
         @ConnectedSocket() client: Socket,
     ) {
+        console.log(`supabaseGatway -> properties-unsubscribe ${client.id}`);
         client.data.propertiesFilter = undefined;
     }
 
@@ -28,6 +30,7 @@ export class SupabaseGateway {
       @MessageBody() data: any,
       @ConnectedSocket() client: Socket,
     ) {
+      console.log(`supabaseGatway -> realtors-subscribe ${client.id}`);
       client.data.realtorsFilter = data;
     }
 
@@ -35,6 +38,7 @@ export class SupabaseGateway {
     realtorUnsubscribe(
       @ConnectedSocket() client: Socket,
     ) {
+      console.log(`supabaseGatway -> realtors-unsubscribe ${client.id}`);
       client.data.realtorsFilter = undefined;
     }
 
@@ -43,6 +47,7 @@ export class SupabaseGateway {
       @MessageBody() data: any,
       @ConnectedSocket() client: Socket,
     ) {
+      console.log(`supabaseGatway -> persons-subscribe ${client.id}`);
       client.data.personsFilter = data;
     }
 
@@ -50,17 +55,22 @@ export class SupabaseGateway {
     personsUnsubscribe(
       @ConnectedSocket() client: Socket,
     ) {
+      console.log(`supabaseGatway -> persons-unsubscribe ${client.id}`);
       client.data.personsFilter = undefined;
     }
 
     notifyCreatedProperty(newProperty: PropertyData) {
+        console.log(`supabaseGateway -> notifyCreatedProperty ${newProperty.id}`);
+
         this.wsServer.sockets.sockets.forEach((client: Socket) => {
             //if newProperty complies client.data.propertiesFilter, send event
             client.emit('new-property', newProperty); 
         })
     }
 
-    notifyUpdatedProperty(updatedProperty: PropertyData) {
+    notifyUpdatedProperty(updatedProperty: PropertyData) { 
+        console.log(`supabaseGateway -> notifyUpdatedProperty ${updatedProperty.id}`);
+
         this.wsServer.sockets.sockets.forEach((client: Socket) => {
             //if updatedProperty complies client.data.propertiesFilter, send event
             client.emit('updated-property', updatedProperty);
@@ -68,13 +78,17 @@ export class SupabaseGateway {
     }
 
     notifyCreatedRealtor(newRealtor: RealtorData) {
+      console.log(`supabaseGateway -> notifyCreatedRealtor ${newRealtor.id}`);
+
       this.wsServer.sockets.sockets.forEach((client: Socket) => {
         //if applies based on personsFilter
         client.emit('new-realtor', newRealtor);
       })
     }
 
-    notifyUpdatedRealtor(updatedRealtor: RealtorData) {
+    notifyUpdatedRealtor(updatedRealtor: RealtorData) { 
+      console.log(`supabaseGateway -> notifyUpdatedRealtor ${updatedRealtor.id}`);
+
       this.wsServer.sockets.sockets.forEach((client: Socket) => {
         //if applies based on filter
         client.emit('updated-realtor', updatedRealtor);
@@ -82,6 +96,8 @@ export class SupabaseGateway {
     }
 
     notifyCreatedPerson(newPerson: PersonData) {
+      console.log(`supabaseGateway -> notifyCreatedPerson ${newPerson.id}`);
+
       this.wsServer.sockets.sockets.forEach((client: Socket) => {
         //if applies based on filter
         client.emit('created-person', newPerson);
@@ -89,6 +105,8 @@ export class SupabaseGateway {
     }
 
     notifyUpdatedPerson(updatedPerson: PersonData) {
+      console.log(`supabaseGateway -> notifyUpdatedPerson ${updatedPerson.id}`);
+
       this.wsServer.sockets.sockets.forEach((client: Socket) => {
         //if applies based on filter
         client.emit('updated-person', updatedPerson);
